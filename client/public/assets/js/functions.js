4334df13be79da4e417e89e4eeba15195f109eb0
Luc Bronsdijk <info@lucbronsdijk.nl>
2014-03-08 02:40:24 +0100

    Add landing animations

--- client/public/assets/js/functions.js
+++ client/public/assets/js/functions.js
@@ -1,11 +1,27 @@
 $(document).ready(function()
 {
+    
+	initLandingAnimate();
     initCaptureAnimate();
+    
+    function initLandingAnimate() 
+    {
+    	$('h1').addClass('animated fadeInDown');
+    }
 
     function initCaptureAnimate() 
     {
 		$('button.record').click(function() {
 			$('.progress-wrapper').css('display', 'block');
+
+			setTimeout(function() {
+				$('h1').removeClass('fadeInDown');
+				$('h1').addClass('fadeOutUp');
+				$('.progress-wrapper').fadeOut('fast', function() {
+					$('button.record').addClass('animated flipOutY');
+				})
+         	}, 10000);
+
 		})
     }
 })
