26138c5dea15f13cb2eb11969b9406d690940bdf
Luc Bronsdijk <info@lucbronsdijk.nl>
2014-03-08 11:48:09 +0100

    Added other users animation

--- client/public/assets/js/functions.js
+++ client/public/assets/js/functions.js
@@ -3,6 +3,7 @@ $(document).ready(function()
     
 	initLandingAnimate();
     initCaptureAnimate();
+    initOtherUsers();
     
     function initLandingAnimate() 
     {
@@ -23,5 +24,22 @@ $(document).ready(function()
          	}, 10000);
 
 		})
+    }
+
+    function initOtherUsers()
+    {
+	    var songs = $(".song");
+	    var songsIndex = -1;
+	    
+	    function showNextSong() {
+	        ++songsIndex;
+	        songs.eq(songsIndex % songs.length)
+	        	.addClass('animated fadeInUp')
+	            .css('display', 'block')
+	            .delay(5000)
+	            .hide(0,0,showNextSong);
+	    }
+	    
+	    showNextSong();
     }
 })
