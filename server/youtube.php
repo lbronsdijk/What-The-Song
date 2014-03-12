628bc923582e75a2e69f7c66dc4c6b20758fc6bb
Lloyd Keijzer <lloyd_msn@live.nl>
2014-03-12 10:18:17 +0100

    Ordering youtube video and performance boost

--- server/youtube.php
+++ server/youtube.php
@@ -1,48 +1,40 @@
 <?php
 
-if (isset($_GET['title']) && isset($_GET['artist'])) {
-    // Call set_include_path() as needed to point to your client library.
-    require_once 'google/client.php';
-    require_once 'google/service/Google_YouTubeService.php';
+if (!isset($_GET['title']))
+    exit();
 
-    /* Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
-    Google APIs Console <http://code.google.com/apis/console#access>
-    Please ensure that you have enabled the YouTube Data API for your project. */
-    $DEVELOPER_KEY = 'AIzaSyDwWG3yyOZ7PwcspzPx65xfPiQp7QPlIz8';
+// Call set_include_path() as needed to point to your client library.
+require_once 'google/client.php';
+require_once 'google/service/Google_YouTubeService.php';
 
-    $client = new Google_Client();
-    $client->setDeveloperKey($DEVELOPER_KEY);
+/* Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+Google APIs Console <http://code.google.com/apis/console#access>
+Please ensure that you have enabled the YouTube Data API for your project. */
+$DEVELOPER_KEY = 'AIzaSyDwWG3yyOZ7PwcspzPx65xfPiQp7QPlIz8';
 
-    $youtube = new Google_YoutubeService($client);
+$client = new Google_Client();
+$client->setDeveloperKey($DEVELOPER_KEY);
 
-    $searchResponse = $youtube->search->listSearch('id,snippet', array(
-        'q' => $_GET['title'] . ' by ' . $_GET['artist'],
-        'maxResults' => 10
-    ));
+$youtube = new Google_YoutubeService($client);
 
-    $videoID = '';
-    $mostViews = 0;
+$searchResponse = $youtube->search->listSearch('id,snippet', array(
+    'q' => $_GET['title'],
+    'maxResults' => 1,
+    'type' => 'video',
+    'order' => 'viewCount'
+));
 
-    foreach ($searchResponse['items'] as $searchResult) {
-        switch ($searchResult['id']['kind']) {
-            case 'youtube#video':
+$videoID = (isset($searchResponse['items'][0])) ? $searchResponse['items'][0]['id']['videoId'] : '';
 
-                $JSON = file_get_contents("https://gdata.youtube.com/feeds/api/videos/{$searchResult['id']['videoId']}?v=2&alt=json");
-                $JSON_Data = json_decode($JSON);
-
-                $views = $JSON_Data->{'entry'}->{'yt$statistics'}->{'viewCount'};
-
-                if($views > $mostViews){
-                    $videoID = $searchResult['id']['videoId'];
-                    $mostViews = $views;
-                }
-
-                break;
-        }
-    }
-
-    if($videoID != '')
-        print '<iframe width="560" height="315" src="//www.youtube.com/embed/'.$videoID.'?autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>';
-    else
-        print 'no videos were found :(';
-}
+if($videoID != ''):
+?>
+<!doctype html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <iframe width="560" height="315" src="//www.youtube.com/embed/<?php print $videoID; ?>?autoplay=1&rel=0" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
+<?php endif; ?>
