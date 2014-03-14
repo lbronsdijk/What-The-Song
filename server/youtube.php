b6d63571571bf88d194dc04aa67df46e5d0fa52f
Lloyd Keijzer <lloyd_msn@live.nl>
2014-03-14 10:11:51 +0100

    pluses instead of spaces.

--- server/youtube.php
+++ server/youtube.php
@@ -18,7 +18,7 @@ $client->setDeveloperKey($DEVELOPER_KEY);
 $youtube = new Google_YoutubeService($client);
 
 $searchResponse = $youtube->search->listSearch('id,snippet', array(
-    'q' => $_GET['title'] . ' by ' . $_GET['artist'],
+    'q' => $_GET['title'] . '+by+' . $_GET['artist'],
     'maxResults' => 1,
     'type' => 'video',
     'order' => 'viewCount'
